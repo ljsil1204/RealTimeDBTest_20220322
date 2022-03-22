febaa5f7990d5eeee38e76b35d1fb5e16bd799df
이진실 <ljsil1204@naver.com>
2022-03-22 09:45:45 +0900

    Initial commit

---
+++ app/src/main/java/com/leejinsil/realtimedbtest_20220322/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.leejinsil.realtimedbtest_20220322
+
+import android.os.Bundle
+
+class MainActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+}
