b6876065bdccfca8821937321711f15139b834e9
이진실 <ljsil1204@naver.com>
2022-03-22 10:08:01 +0900

    채팅 UI 설정   - DB 연결 정보를 BaseActivity에서 상속

--- app/src/main/java/com/leejinsil/realtimedbtest_20220322/BaseActivity.kt
+++ app/src/main/java/com/leejinsil/realtimedbtest_20220322/BaseActivity.kt
@@ -3,10 +3,14 @@ package com.leejinsil.realtimedbtest_20220322
 import android.content.Context
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.FirebaseDatabase
 
 abstract class BaseActivity : AppCompatActivity() {
 
     lateinit var mContext : Context
+
+//    DB 서버 주소
+    val realtimeDB = FirebaseDatabase.getInstance("https://realtimedbtest20220322-default-rtdb.asia-southeast1.firebasedatabase.app/")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
